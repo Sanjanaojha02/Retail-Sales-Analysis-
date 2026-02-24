dc3ea725a01e623d796928b81ebe20b557af88ff
Sanjanaojha02 <sanjanaojha0205@gmail.com>
2026-02-24 23:22:28 +0530

    SQL retail sales analysis.

--- sql retail sales analysis.sql
+++ sql retail sales analysis.sql
@@ -1,11 +1,5 @@
                            #SQL RETAIL SALES ANALYSIS
-		#INTRODUCTION: The retails sales analysis was done using SQL {STRUCTURED QUERY LANGUAGE} to optain meaningful insights.The motive of this project was to get a clean and structured data using sql to enhance more of my skills to understand in depth knowledge about the sql tool.Retail businesses generate large volumes of data daily, including information about products, customers, sales dates and quantity. Analyzing this data helps organizations understand customer purchasing behavior, seasonal trends, product performance, and overall business growth.
-		#OBJECTIVE:
-        -- 1.To import and structure retail sales data into a relational database.
-		-- 2.To perform data cleaning and handle missing or null values.
-         -- 3.To analyze monthly and yearly sales performance.
-         -- 4.To identify best-selling products and top-performing months.
-         -- 5.To generate actionable insights that can help improve sales strategy and business performance.
+		
 
         #RETAIL SALES ANALYSIS PROJECT
 CREATE DATABASE Retail_analysis
